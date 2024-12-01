4c5e69148faef870199b604b3c2136af41739251
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-01 13:24:30 -0500

    WIP

---
+++ Days/DayTemplate.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace advent2024;
+
+public class DayTemplate : BaseDay
+{
+    public DayTemplate(int day)
+        : base(day) { }
+
+    protected override string SolveFirst()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string SolveSecond()
+    {
+        throw new NotImplementedException();
+    }
+}
