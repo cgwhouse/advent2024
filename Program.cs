6b1bb1e1b57512c566bd6136c0de4070ba542d1e
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-01 13:20:05 -0500

    WIP

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using advent2024;
 
 if (!int.TryParse(args[0], out var day) || !int.TryParse(args[1], out var part))
 {
@@ -11,15 +12,15 @@ var dayType =
     ?? throw new Exception($"Unable to find implementation for day {day}");
 
 var dayInstance =
-    (BaseDay?)Activator.CreateInstance(dayType)
+    (BaseDay?)Activator.CreateInstance(dayType, day)
     ?? throw new Exception($"Unable to calculate solution for day {day}");
 
 string result;
 
 result = part switch
 {
-    1 => (string)dayInstance.FirstResult,
-    2 => (string)dayInstance.SecondResult,
+    1 => dayInstance.FirstResult,
+    2 => dayInstance.SecondResult,
     _ => throw new Exception(
         "Provide a 1 or 2 for the second argument (part 1 or 2 of the solution)"
     ),
---
+++ days/BaseDay.cs
@@ -0,0 +1,21 @@
+using System.IO;
+
+namespace advent2024;
+
+public abstract class BaseDay
+{
+    protected string[] InputFromFile;
+    public string FirstResult => SolveFirst();
+    public string SecondResult => SolveSecond();
+
+    public BaseDay(int day) => InputFromFile = File.ReadAllLines($"inputs/day{day}.txt");
+
+    protected abstract string SolveFirst();
+    protected abstract string SolveSecond();
+}
+
+//public interface IDay
+//{
+//    public string SolveFirst();
+//    public string SolveSecond();
+//}
