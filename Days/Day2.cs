2e90b6f1d7e7c17386d09703cdc1fbf524e11530
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-02 01:09:12 -0500

    day 2 part 1

--- Days/Day2.cs
+++ Days/Day2.cs
@@ -23,19 +23,26 @@ public class Day2(int day) : BaseDay(day)
             if (nums[0] > nums[1])
                 increasing = false;
 
+            var valid = true;
+
             for (int i = 0; i < nums.Count - 1; i++)
             {
-                if (increasing && nums[i] >= nums[i + 1])
-                    continue;
+                var diff = Math.Abs(nums[i] - nums[i + 1]);
 
-                if (!increasing && nums[i] <= nums[i + 1])
-                    continue;
-
-                if (Math.Abs(nums[i] - nums[i + 1]) < 1 || Math.Abs(nums[i] - nums[i + 1]) > 3)
-                    continue;
+                if (
+                    increasing && nums[i] >= nums[i + 1]
+                    || !increasing && nums[i] <= nums[i + 1]
+                    || diff < 1
+                    || diff > 3
+                )
+                {
+                    valid = false;
+                    break;
+                }
             }
 
-            result++;
+            if (valid)
+                result++;
         }
 
         return result.ToString();
