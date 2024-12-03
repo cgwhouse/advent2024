a320068a1936174a51dda8f4b81ff7a41249bfa5
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-03 00:40:01 -0500

    part 1

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace advent2024.Days;
 
@@ -6,7 +7,51 @@ public class Day3(int day) : BaseDay(day)
 {
     protected override string SolveFirst()
     {
-        throw new NotImplementedException();
+        var result = 0;
+
+        foreach (var line in InputFromFile)
+        {
+            for (int i = 0; i < line.Count(); i++)
+            {
+                if (i + 4 >= line.Count())
+                    break;
+
+                if (line.Substring(i, 4) != "mul(")
+                    continue;
+
+                // See if we get several ints followed by a comma
+                var first = "";
+                var curr = i + 4;
+
+                while (int.TryParse(line[curr].ToString(), out var digit))
+                {
+                    first += digit.ToString();
+                    curr++;
+                }
+
+                // We need to now be on a comma, with ints accumulated
+                if (line[curr].ToString() != "," || !int.TryParse(first, out var firstInt))
+                    continue;
+
+                // Move past the comma and try to collect secondInt
+                curr++;
+
+                var second = "";
+
+                while (int.TryParse(line[curr].ToString(), out var digit))
+                {
+                    second += digit.ToString();
+                    curr++;
+                }
+
+                if (line[curr].ToString() != ")" || !int.TryParse(second, out var secondInt))
+                    continue;
+
+                result += firstInt * secondInt;
+            }
+        }
+
+        return result.ToString();
     }
 
     protected override string SolveSecond()
