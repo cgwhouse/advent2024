df6a4e8586ed5eb8129340c043d1febb51fae5fd
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-05 12:01:01 -0500

    wip part 2

--- Days/Day5.cs
+++ Days/Day5.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace advent2024.Days;
 
@@ -7,12 +8,6 @@ public class Day5(int day) : BaseDay(day)
 {
     protected override string SolveFirst()
     {
-        // dictionary of key, value is list of things that must come after
-        // for each number in the list of pages
-        // look at everything up until the number we're currently on
-        // if any of those numbers leading up to current number are in the "must come after"
-        // list for that number, it is not correct
-
         var rulesDict = new Dictionary<int, List<int>>();
         var validUpdates = new List<List<int>>();
 
@@ -82,6 +77,126 @@ public class Day5(int day) : BaseDay(day)
 
     protected override string SolveSecond()
     {
-        throw new NotImplementedException();
+        var rulesDict = new Dictionary<int, List<int>>();
+        var invalidUpdates = new List<List<int>>();
+
+        foreach (var line in InputFromFile)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+
+            if (line.Contains('|'))
+            {
+                var rule = line.Split('|');
+                var first = int.Parse(rule[0]);
+                var second = int.Parse(rule[1]);
+
+                if (rulesDict.ContainsKey(first))
+                    rulesDict[first].Add(second);
+                else
+                    rulesDict[first] = [second];
+
+                continue;
+            }
+
+            var pageUpdate = line.Split(',');
+
+            var processedUpdate = new List<int>();
+            var valid = true;
+
+            foreach (var pu in pageUpdate)
+            {
+                var pageNum = int.Parse(pu);
+
+                if (!rulesDict.ContainsKey(pageNum))
+                {
+                    processedUpdate.Add(pageNum);
+                    continue;
+                }
+
+                var rules = rulesDict[pageNum];
+
+                foreach (var rule in rules)
+                {
+                    if (processedUpdate.Contains(rule))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (!valid)
+                    break;
+
+                processedUpdate.Add(pageNum);
+            }
+
+            if (!valid)
+                invalidUpdates.Add(processedUpdate);
+        }
+
+        var validUpdates = new List<List<int>>();
+
+        foreach (var invalidUpdate in invalidUpdates)
+        {
+            var validUpdate = new List<int>();
+
+            foreach (var pageNum in invalidUpdate)
+            {
+                // No rules to follow, or this is the first one
+                if (!rulesDict.ContainsKey(pageNum) || validUpdate.Count == 0)
+                {
+                    validUpdate.Add(pageNum);
+                    continue;
+                }
+
+                // Get the rules for our current pageNum we're trying to add
+                var rules = rulesDict[pageNum];
+
+                // We will look for its location in the list we're currently building
+                var currentDesiredIndex = 0;
+                var locationFound = false;
+
+                while (!locationFound)
+                {
+                    // We may have already hit the end
+                    if (currentDesiredIndex == validUpdate.Count)
+                    {
+                        locationFound = true;
+                        validUpdate.Add(pageNum);
+                    }
+
+                    var test = new List<int>();
+                    test.AddRange(validUpdate);
+                    test.Insert(currentDesiredIndex, pageNum);
+
+                    var slice = test.Slice(
+                        currentDesiredIndex + 1,
+                        test.Count - currentDesiredIndex - 1
+                    );
+
+                    if (!slice.Any(x => rules.Contains(x)))
+                    {
+                        locationFound = true;
+                        validUpdate = test;
+                    }
+                    else
+                    {
+                        currentDesiredIndex++;
+                    }
+                }
+            }
+
+            // TODO: reorder the invalid update
+
+            validUpdates.Add(validUpdate.ToList());
+        }
+
+        var result = 0;
+
+        foreach (var update in validUpdates)
+            result += update[update.Count / 2];
+
+        return result.ToString();
     }
 }
