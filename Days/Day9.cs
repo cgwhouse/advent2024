bfa9bfc646deda2a92978a33e3653db7eb0dd3e6
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-09 18:22:40 -0500

    wip

--- Days/Day9.cs
+++ Days/Day9.cs
@@ -8,6 +8,32 @@ public class Day9(int day) : BaseDay(day)
     protected override string SolveFirst()
     {
         var diskMap = InputFromFile.First() ?? throw new Exception();
+        var diskMapExpanded = "";
+
+        var currentID = 0;
+        var isFile = true;
+
+        for (int i = 0; i < diskMap.Length; i++)
+        {
+            var sizeOfThing = diskMap[i];
+
+            // Add however many chars to the expanded disk map based on the size
+            for (int j = 0; j < sizeOfThing; j++)
+            {
+                // If we're currently doing a file add the ID we're on
+                // Otherwise just do dots
+                if (isFile)
+                {
+                    diskMapExpanded += currentID.ToString();
+                }
+                else
+                {
+                    diskMapExpanded += '.';
+                }
+            }
+        }
+
+        Console.WriteLine(diskMapExpanded);
 
         throw new NotImplementedException();
     }
