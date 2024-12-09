711c2c507ac2cbf61cbfca94e7d6fc1f4b61ba7a
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-09 14:22:44 -0500

    initial day 9

---
+++ Days/Day9.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace advent2024.Days;
+
+public class Day9(int day) : BaseDay(day)
+{
+    protected override string SolveFirst()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string SolveSecond()
+    {
+        throw new NotImplementedException();
+    }
+}
