b7e47e3cf7b5593975d90c668c159b4c283f0560
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-08 21:15:45 -0500

    day 8 part 1

--- Days/Day8.cs
+++ Days/Day8.cs
@@ -9,9 +9,7 @@ public class Day8(int day) : BaseDay(day)
     {
         var board = new List<char[]>();
         foreach (var line in InputFromFile)
-        {
             board.Add(line.ToCharArray());
-        }
 
         var stationDict = new Dictionary<char, List<(int, int)>>();
 
@@ -23,15 +21,15 @@ public class Day8(int day) : BaseDay(day)
                 {
                     var stationChar = board[i][j];
 
-                    if (stationDict.ContainsKey(stationChar))
-                        stationDict[stationChar].Add((i, j));
+                    if (stationDict.TryGetValue(stationChar, out var stationList))
+                        stationList.Add((i, j));
                     else
-                        stationDict[stationChar] = new List<(int, int)>() { (i, j) };
+                        stationDict[stationChar] = [(i, j)];
                 }
             }
         }
 
-        var antinodes = new List<(int, int)>();
+        var possibleAntinodes = new List<(int Row, int Column)>();
 
         foreach (var stationChar in stationDict.Keys)
         {
@@ -41,6 +39,7 @@ public class Day8(int day) : BaseDay(day)
 
             // Take each combo of stations of this type and determine their antinodes
             var stationCoords = stationDict[stationChar];
+
             for (int i = 0; i < stationCoords.Count; i++)
             {
                 for (int j = 0; j < stationCoords.Count; j++)
@@ -48,47 +47,39 @@ public class Day8(int day) : BaseDay(day)
                     if (j == i)
                         continue;
 
-                    antinodes.AddRange(DetermineAntinodes(stationCoords[i], stationCoords[j]));
+                    possibleAntinodes.AddRange(
+                        DetermineAntinodes(stationCoords[i], stationCoords[j])
+                    );
                 }
             }
         }
 
-        var uniqueAntinodeLocations = new HashSet<(int, int)>();
+        var uniqueAntinodeLocations = new HashSet<(int Row, int Column)>();
 
-        foreach (var possibleAntinode in antinodes)
+        foreach (var (Row, Column) in possibleAntinodes)
         {
             try
             {
-                var test = board[possibleAntinode.Item1][possibleAntinode.Item2];
-                uniqueAntinodeLocations.Add((possibleAntinode.Item1, possibleAntinode.Item2));
-            }
-            catch (ArgumentOutOfRangeException)
-            {
+                var _ = board[Row][Column];
 
+                uniqueAntinodeLocations.Add((Row, Column));
             }
+            catch (Exception ex)
+                when (ex is ArgumentOutOfRangeException || ex is IndexOutOfRangeException) { }
         }
-
-        Console.WriteLine("here");
 
         return uniqueAntinodeLocations.Count.ToString();
     }
 
-    private List<(int, int)> DetermineAntinodes(
+    private static List<(int Row, int Column)> DetermineAntinodes(
         (int Row, int Column) firstPoint,
         (int Row, int Column) secondPoint
     )
     {
-        if (firstPoint.Row == 2 && firstPoint.Column == 5 && secondPoint.Row == 4 && secondPoint.Column == 4)
-        {
-            Console.WriteLine("here");
-        }
-
         var result = new List<(int, int)>();
-        // (x, y) is row, column indexes in the list of lists?
-        //
-        // points (3, 4) and (5, 5) create antinodes (1, 3) and (7, 6)
-        var rowDiff = Math.Abs(firstPoint.Row - secondPoint.Row); // 2
-        var colDiff = Math.Abs(firstPoint.Column - secondPoint.Column); // 1
+
+        var rowDiff = Math.Abs(firstPoint.Row - secondPoint.Row);
+        var colDiff = Math.Abs(firstPoint.Column - secondPoint.Column);
 
         // Take first point and create the antinode using it as a frame of reference
         // Start at the second point and "move to" the first point, but double the values
@@ -96,30 +87,18 @@ public class Day8(int day) : BaseDay(day)
         int firstAntiColumn;
 
         if (firstPoint.Row < secondPoint.Row)
-        {
             firstAntiRow = secondPoint.Row - (rowDiff * 2);
-        }
         else if (firstPoint.Row > secondPoint.Row)
-        {
             firstAntiRow = secondPoint.Row + (rowDiff * 2);
-        }
         else
-        {
             firstAntiRow = firstPoint.Row;
-        }
 
         if (firstPoint.Column < secondPoint.Column)
-        {
             firstAntiColumn = secondPoint.Column - (colDiff * 2);
-        }
         else if (firstPoint.Column > secondPoint.Column)
-        {
             firstAntiColumn = secondPoint.Column + (colDiff * 2);
-        }
         else
-        {
             firstAntiColumn = firstPoint.Column;
-        }
 
         result.Add((firstAntiRow, firstAntiColumn));
 
@@ -128,30 +107,18 @@ public class Day8(int day) : BaseDay(day)
         int secondAntiColumn;
 
         if (secondPoint.Row < firstPoint.Row)
-        {
             secondAntiRow = firstPoint.Row - (rowDiff * 2);
-        }
         else if (secondPoint.Row > firstPoint.Row)
-        {
             secondAntiRow = firstPoint.Row + (rowDiff * 2);
-        }
         else
-        {
             secondAntiRow = firstPoint.Row;
-        }
 
         if (secondPoint.Column < firstPoint.Column)
-        {
             secondAntiColumn = firstPoint.Column - (colDiff * 2);
-        }
         else if (secondPoint.Column > firstPoint.Column)
-        {
             secondAntiColumn = firstPoint.Column + (colDiff * 2);
-        }
         else
-        {
             secondAntiColumn = firstPoint.Column;
-        }
 
         result.Add((secondAntiRow, secondAntiColumn));
 
