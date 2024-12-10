bf141b24408643d022d37cfa056e2eb1e647b014
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-09 19:20:20 -0500

    wip

--- Days/Day9.cs
+++ Days/Day9.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace advent2024.Days;
@@ -32,9 +33,52 @@ public class Day9(int day) : BaseDay(day)
             isFile = !isFile;
         }
 
-        Console.WriteLine(diskMapExpanded);
+        // As we encounter free space while moving left to right,
+        // keep track of where we are as we're pulling values off of the right side
+        var currentRight = diskMapExpanded.Length - 1;
+        while (diskMapExpanded[currentRight] == '.')
+            currentRight--;
 
-        throw new NotImplementedException();
+        var compactedDiskMap = "";
+        var indicesAlreadyCompacted = new HashSet<int>();
+
+        for (int i = 0; i < diskMapExpanded.Length; i++)
+        {
+            if (i > currentRight || indicesAlreadyCompacted.Contains(i))
+            {
+                compactedDiskMap += '.';
+                continue;
+            }
+
+            if (diskMapExpanded[i] != '.')
+            {
+                compactedDiskMap += diskMapExpanded[i];
+                continue;
+            }
+
+            // We have a free space, pull something off the right, then add to indices
+            compactedDiskMap += diskMapExpanded[currentRight];
+            indicesAlreadyCompacted.Add(currentRight);
+
+            // Find the next thing on the right we can swap into free space
+            currentRight--;
+            while (diskMapExpanded[currentRight] == '.')
+                currentRight--;
+        }
+
+        //Console.WriteLine(compactedDiskMap);
+
+        long result = 0;
+
+        for (int i = 0; i < compactedDiskMap.Length; i++)
+        {
+            if (compactedDiskMap[i] == '.')
+                continue;
+
+            result += int.Parse(compactedDiskMap[i].ToString()) * i;
+        }
+
+        return result.ToString();
     }
 
     protected override string SolveSecond()
