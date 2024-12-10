9fb0fc10e8c0af5ca945f3e53cb17a0c211db463
Cristian Widenhouse <cgwidenhouse@gmail.com>
2024-12-10 14:10:16 -0500

    wip day 10

--- Days/Day10.cs
+++ Days/Day10.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace advent2024.Days;
 
@@ -6,7 +7,35 @@ public class Day10(int day) : BaseDay(day)
 {
     protected override string SolveFirst()
     {
-        throw new NotImplementedException();
+        var board = new List<List<int>>();
+
+        foreach (var line in InputFromFile)
+        {
+            var row = new List<int>();
+
+            foreach (var n in line)
+                row.Add(n);
+
+            board.Add(row);
+        }
+
+        // Keys are (x,y) of trailheads
+        // Values are the 9s that can be reached from each trailhead key
+        var trailheadDict = new Dictionary<(int Row, int Column), HashSet<(int Row, int Column)>>();
+
+        // Gather trailheads as keys
+        for (int i = 0; i < board.Count; i++)
+        {
+            for (int j = 0; j < board[i].Count; j++)
+            {
+                if (board[i][j] == 0)
+                    trailheadDict[(i, j)] = new HashSet<(int Row, int Column)>();
+            }
+        }
+
+        var result = 0;
+
+        return result.ToString();
     }
 
     protected override string SolveSecond()
